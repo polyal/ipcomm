7e2e31a7ffafc697760f3fbd2f976461c19158af
Lorant Polya <lorant.polya@gmail.com>
2021-01-22 18:24:12 -0500

    cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -5,55 +5,59 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-
-#define PORT 8080
+#include <string>
 
 using namespace std;
 
-void server()
+
+const unsigned short port = 8080;
+const string hello = "Hello from server";
+
+
+int server()
 { 
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
        
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){ 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
+        cout << "ERR socket: " << errno << endl; 
+        return errno;
     } 
        
     // Forcefully attaching socket to the port 8080 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){ 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
+        cout << "ERR setsockopt: " << errno << endl; 
+        return errno; 
     } 
 
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT );    
+    address.sin_port = htons( port );    
     // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){ 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){ 
+        cout << "ERR bind: " << errno << endl;
+        return errno;
     } 
 
     if (listen(server_fd, 3) < 0){ 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
+        cout << "ERR listen: " << errno << endl; 
+        return errno;
     } 
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){ 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0){ 
+        cout << "ERR accept: " << errno << endl; 
+        return errno;
     }
 
     valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n");
+    cout << "Message: " << buffer << endl;
+    send(new_socket , hello.data() , hello.length(), 0 ); 
+    cout << "Hello message sent" << endl;
+    return 0;
 }
 
 
